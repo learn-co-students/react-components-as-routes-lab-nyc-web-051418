a3f00d0e2c80b2a5b2537f9e180889ff45a9ddb7
github username <github email address>
2018-07-23 14:24:02 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import { directors } from '../data';
+import UUID from "uuid"
+// const Directors = () => {
+//   return (
+//     <div>
+//       {/*{code here}*/}
+//     </div>
+//   );
+// }
+//
+// export default Directors
 
-const Directors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
+
+
+export default class Directors extends React.Component {
+  listMovies = (movies) => {
+    return movies.map(movie=>{
+      return(<ul key={UUID()}>
+        <li>{movie}</li>
+      </ul>)
+    })
+  }
+  handleDirectors = () => {
+ return directors.map(director=>{
+      return (<div key={UUID()}><h2>Name: {director.name}</h2>
+      {this.listMovies(director.movies)}
+      </div>)
+    })
+  }
+render() {
+return (
+ <div>
+<h1>Directors Page</h1>
+{this.handleDirectors()}
+</div> )
 }
-
-export default Directors
+}
